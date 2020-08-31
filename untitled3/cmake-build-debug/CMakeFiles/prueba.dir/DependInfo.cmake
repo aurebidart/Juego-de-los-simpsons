688c00586bfa1c297f8d1b5c58579833c9e851fd
aurebidart <52515878+aurebidart@users.noreply.github.com>
2020-08-31 19:16:34 -0300

    Add files via upload

---
+++ untitled3/cmake-build-debug/CMakeFiles/prueba.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/aureb/CLionProjects/untitled3/Comida.cpp" "C:/Users/aureb/CLionProjects/untitled3/cmake-build-debug/CMakeFiles/prueba.dir/Comida.cpp.obj"
+  "C:/Users/aureb/CLionProjects/untitled3/Corazon.cpp" "C:/Users/aureb/CLionProjects/untitled3/cmake-build-debug/CMakeFiles/prueba.dir/Corazon.cpp.obj"
+  "C:/Users/aureb/CLionProjects/untitled3/homer.cpp" "C:/Users/aureb/CLionProjects/untitled3/cmake-build-debug/CMakeFiles/prueba.dir/homer.cpp.obj"
+  "C:/Users/aureb/CLionProjects/untitled3/main.cpp" "C:/Users/aureb/CLionProjects/untitled3/cmake-build-debug/CMakeFiles/prueba.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
